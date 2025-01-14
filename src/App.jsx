be86d8a70ad7da35a50f91de04933f2edd4959af
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2025-01-14 19:32:15 +0530

    Created Responsive Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import './App.css'
+import HeroSection from './components/HeroSection'
+import Navbar from './components/Navbar'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Navbar/>
+    <HeroSection/>
+    </>
   )
 }

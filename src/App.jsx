497ed0121d8b35269add2911d5be91d28a853233
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2025-01-17 22:39:38 +0530

    Created Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
+import FeatureSection from './components/FeatureSection'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <>
-    <Navbar/>
-    <HeroSection/>
+      <Navbar />
+      <div className='max-w-7xl mx-auto pt-20 px-6'>
+        <HeroSection />
+
+      </div>
+      {/* <FeatureSection/> */}
     </>
   )
 }

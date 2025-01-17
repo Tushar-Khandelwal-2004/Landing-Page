b82034b39699b5ac25bce14ce980e3fa47c2152b
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2025-01-17 23:04:53 +0530

    Created Features Section

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ export default function App() {
       <Navbar />
       <div className='max-w-7xl mx-auto pt-20 px-6'>
         <HeroSection />
+        <FeatureSection />
 
       </div>
-      {/* <FeatureSection/> */}
     </>
   )
 }
